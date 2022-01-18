44b7f63d5726d41922eefd9bab5ca8ad204487ba
Nilton F J <niltonfjr086@gmail.com>
2022-01-18 20:09:12 -0300

    feat: implement "validateBrackets" method

--- src/service/string-validation/string-validation.service.ts
+++ src/service/string-validation/string-validation.service.ts
@@ -2,7 +2,54 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class StringValidationService {
+  private validOpenChars = ['(', '{', '['];
+  private validCloseChars = [')', '}', ']'];
+
+  private getCurrentCharIndex(input: string) {
+    return this.validOpenChars.indexOf(input[0]);
+  }
+
+  private getCurrentCloseChar(currentCharIndex: number) {
+    return this.validCloseChars[currentCharIndex];
+  }
+
+  private getCurrentCloseCharIndex(input: string, currentCloseChar: string) {
+    return input.indexOf(currentCloseChar);
+  }
+
+  private getResidualInput(input: string, currentCloseCharIndex: number) {
+    return input
+      .replace(input[0], '')
+      .replace(input[currentCloseCharIndex], '');
+  }
+
+  private validateBrackets(input: string): boolean {
+    const currentCharIndex = this.getCurrentCharIndex(input);
+    if (currentCharIndex < 0) {
+      return false;
+    }
+
+    const currentCloseChar = this.getCurrentCloseChar(currentCharIndex);
+    const currentCloseCharIndex = this.getCurrentCloseCharIndex(
+      input,
+      currentCloseChar,
+    );
+    if (currentCloseCharIndex < 0) {
+      return false;
+    }
+
+    const residualInput = this.getResidualInput(input, currentCloseCharIndex);
+    if (residualInput.length > 0) {
+      return this.validateBrackets(residualInput);
+    }
+    return true;
+  }
+
   hasValidBalancedBrackets(input: string): boolean {
-    return !!input;
+    const couldValidate = input && typeof input === 'string';
+    if (!couldValidate) {
+      return false;
+    }
+    return this.validateBrackets(input);
   }
 }
