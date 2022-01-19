a0f76f81cfeef4b1e6b9ff66fde54cb2dc135204
Nilton F J <niltonfjr086@gmail.com>
2022-01-19 00:07:40 -0300

    feat: add ORM and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StringValidationService } from './services/string-validation/string-validation.service';
 import { StringValidationController } from './controllers/string-validation/string-validation.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'node-db',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true, // Do not use in production
+    }),
+  ],
   controllers: [AppController, StringValidationController],
   providers: [AppService, StringValidationService],
 })
